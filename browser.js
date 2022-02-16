765848496b627f4919d598074669cb81087b6d24
janniks <janniks@users.noreply.github.com>
2022-02-16 17:47:32 +0100

    update project

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 // limit of Crypto.getRandomValues()
 // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
@@ -8,45 +8,50 @@ const MAX_BYTES = 65536;
 // https://github.com/nodejs/node/blob/master/lib/internal/crypto/random.js#L48
 const MAX_UINT32 = 4294967295;
 
-function oldBrowser () {
-  throw new Error('Secure random number generation is not supported by this browser.\nUse Chrome, Firefox or Internet Explorer 11')
+function oldBrowser() {
+  throw new Error(
+    "Secure random number generation is not supported by this browser.\nUse Chrome, Firefox or Internet Explorer 11"
+  );
 }
 
-const Buffer = require('safe-buffer').Buffer;
+const Buffer = require("safe-buffer").Buffer;
 
-const global = typeof window !== 'undefined' ? window : global;
+const global = typeof window !== "undefined" ? window : global;
 const crypto = global.crypto || global.msCrypto;
 
 if (crypto && crypto.getRandomValues) {
-  module.exports = randomBytes
+  module.exports = randomBytes;
 } else {
-  module.exports = oldBrowser
+  module.exports = oldBrowser;
 }
 
-function randomBytes (size, cb) {
+function randomBytes(size, cb) {
   // phantomjs needs to throw
-  if (size > MAX_UINT32) throw new RangeError('requested too many random bytes')
+  if (size > MAX_UINT32)
+    throw new RangeError("requested too many random bytes");
 
   const bytes = Buffer.allocUnsafe(size);
 
-  if (size > 0) {  // getRandomValues fails on IE if size == 0
-    if (size > MAX_BYTES) { // this is the max bytes crypto.getRandomValues
+  if (size > 0) {
+    // getRandomValues fails on IE if size == 0
+    if (size > MAX_BYTES) {
+      // this is the max bytes crypto.getRandomValues
       // can do at once see https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
       for (let generated = 0; generated < size; generated += MAX_BYTES) {
         // buffer.slice automatically checks if the end is past the end of
         // the buffer so we don't have to here
-        crypto.getRandomValues(bytes.slice(generated, generated + MAX_BYTES))
+        crypto.getRandomValues(bytes.slice(generated, generated + MAX_BYTES));
       }
     } else {
-      crypto.getRandomValues(bytes)
+      crypto.getRandomValues(bytes);
     }
   }
 
-  if (typeof cb === 'function') {
+  if (typeof cb === "function") {
     return process.nextTick(function () {
-      cb(null, bytes)
-    })
+      cb(null, bytes);
+    });
   }
 
-  return bytes
+  return bytes;
 }
